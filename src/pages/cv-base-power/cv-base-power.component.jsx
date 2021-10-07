9639d8d13e1d05dcda5e330e88a2c9e8c6291bee
unknown <ericsshyus@gmail.com>
2021-10-07 10:12:53 -0400

    added styling for cv-base-power page

--- src/pages/cv-base-power/cv-base-power.component.jsx
+++ src/pages/cv-base-power/cv-base-power.component.jsx
@@ -29,8 +29,8 @@ class CVBasePower extends React.Component {
   calculateBasePower() {
     const { atk1, atk2, cd1, cd2, cr1, cr2 } = this.state;
     this.setState({
-      bp1: atk1 * (cr1 / 100) * (cd1 / 100),
-      bp2: atk2 * (cr2 / 100) * (cd2 / 100),
+      bp1: (atk1 * (cr1 / 100) * (cd1 / 100)).toFixed(5),
+      bp2: (atk2 * (cr2 / 100) * (cd2 / 100)).toFixed(5),
     });
   }
   handleChange(event) {
@@ -59,88 +59,101 @@ class CVBasePower extends React.Component {
 
     return (
       <div className="cv-base-power">
-        {console.log(this.state)}
-        <h1>artifact crit value </h1>
-        <div className="section">
-          <div className="inputs">
-            <FormInputWithLabel
-              type="number"
-              name="artifactCR"
-              value={artifactCR}
-              label="artifact crit rate"
-              handleChange={this.handleChange}
-            />
+        <h1>relative strength calculator </h1>
+        <div className="content">
+          <h1>artifact crit value </h1>
+          <div className="main-section">
+            <div className="section">
+              <div className="inputs">
+                <FormInputWithLabel
+                  type="number"
+                  name="artifactCR"
+                  value={artifactCR}
+                  label="artifact crit rate"
+                  handleChange={this.handleChange}
+                />
 
-            <FormInputWithLabel
-              type="number"
-              name="artifactCD"
-              value={artifactCD}
-              label="artifact crit damage"
-              handleChange={this.handleChange}
-            />
+                <FormInputWithLabel
+                  type="number"
+                  name="artifactCD"
+                  value={artifactCD}
+                  label="artifact crit damage"
+                  handleChange={this.handleChange}
+                />
+              </div>
+              <div className="results">
+                crit value:<span className="result">{critValue}</span>
+              </div>
+            </div>
           </div>
-          <div className="results">crit value={critValue}</div>
-        </div>
 
-        <h1>base power </h1>
-        <h2> base power 1 </h2>
-        <div className="section">
-          <div className="inputs">
-            <FormInputWithLabel
-              type="number"
-              name="atk1"
-              value={atk1}
-              label="attack"
-              handleChange={this.handleChange}
-            />
+          <h1>base power </h1>
+          <div className="main-section">
+            <h2> base power 1 </h2>
+            <div className="section">
+              <div className="inputs">
+                <FormInputWithLabel
+                  type="number"
+                  name="atk1"
+                  value={atk1}
+                  label="attack"
+                  handleChange={this.handleChange}
+                />
 
-            <FormInputWithLabel
-              type="number"
-              name="cr1"
-              value={cr1}
-              label="crit rate"
-              handleChange={this.handleChange}
-            />
+                <FormInputWithLabel
+                  type="number"
+                  name="cr1"
+                  value={cr1}
+                  label="crit rate"
+                  handleChange={this.handleChange}
+                />
 
-            <FormInputWithLabel
-              type="number"
-              name="cd1"
-              value={cd1}
-              label="crit damage"
-              handleChange={this.handleChange}
-            />
+                <FormInputWithLabel
+                  type="number"
+                  name="cd1"
+                  value={cd1}
+                  label="crit damage"
+                  handleChange={this.handleChange}
+                />
+              </div>
+              <div className="results">
+                base power 1:<span className="result">{bp1}</span>
+              </div>
+            </div>
+
+            <h2>base power 2</h2>
+
+            <div className="section">
+              <div className="inputs">
+                <FormInputWithLabel
+                  type="number"
+                  name="atk2"
+                  value={atk2}
+                  label="attack"
+                  handleChange={this.handleChange}
+                />
+
+                <FormInputWithLabel
+                  type="number"
+                  name="cr2"
+                  value={cr2}
+                  label="crit rate"
+                  handleChange={this.handleChange}
+                />
+
+                <FormInputWithLabel
+                  type="number"
+                  name="cd2"
+                  value={cd2}
+                  label="crit damage"
+                  handleChange={this.handleChange}
+                />
+              </div>
+              <div className="results">
+                base power 2=<span className="result">{bp2}</span>
+              </div>
+            </div>
           </div>
-          <div className="results">base power 1={bp1}</div>
-        </div>
-
-        <h2>base power 2</h2>
-        <div className="section">
-          <div className="inputs">
-            <FormInputWithLabel
-              type="number"
-              name="atk2"
-              value={atk2}
-              label="attack"
-              handleChange={this.handleChange}
-            />
-
-            <FormInputWithLabel
-              type="number"
-              name="cr2"
-              value={cr2}
-              label="crit rate"
-              handleChange={this.handleChange}
-            />
-
-            <FormInputWithLabel
-              type="number"
-              name="cd2"
-              value={cd2}
-              label="crit damage"
-              handleChange={this.handleChange}
-            />
-          </div>
-          <div className="results">base power 2={bp2}</div>
         </div>
       </div>
     );
