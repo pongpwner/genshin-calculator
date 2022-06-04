41d7c0dbafbfcc86b2a3664cc82921740fbc19c7
unknown <ericsshyus@gmail.com>
2022-06-04 00:40:40 -0400

    fixed bug where typing in numbers didnt calculate for weapon calculator

--- src/redux/weapon/weapon.utils.js
+++ src/redux/weapon/weapon.utils.js
@@ -6,17 +6,17 @@ export const WeaponCalculationParams = {
   nBlue: 0,
   nPurple: 0,
   nOrange: 0,
-  // over flow
+  // over flow = number of extra materials of that color, used to be converted to the next level
   oGreen: 0,
   oBlue: 0,
   oPurple: 0,
   oOrange: 0,
-  // underflow
+  // underflow = number of material needed to reach n...
   uGreen: 0,
   uBlue: 0,
   uPurple: 0,
   uOrange: 0,
-  //potential material
+  //potential material=  number of materials converted from the tier below + number of actual blues left over
   //no green
   pBlue: 0,
   pPurple: 0,
@@ -26,7 +26,7 @@ export const WeaponCalculationParams = {
   rBlue: 0,
   rPurple: 0,
   rOrange: 0,
-  //missing material
+  //missing material, shows up as required materials on dom
   mGreen: 0,
   mBlue: 0,
   mPurple: 0,
@@ -97,6 +97,8 @@ export function calculateDomainMaterial(
   WeaponCalculationParams.nBlue = 0;
   WeaponCalculationParams.nPurple = 0;
   WeaponCalculationParams.nOrange = 0;
+  console.log(domainMaterialGreen);
+  //calculate total number of needed materials
   for (
     let i = Number(sumCurrentAscension) + 1;
     i < sumDesiredAscension + 1;
@@ -111,60 +113,89 @@ export function calculateDomainMaterial(
     WeaponCalculationParams.nOrange +=
       WEAPON[rarity].ascension[i].domainCost.orange;
   }
+
   //calculate overflow and underflow
+  //if given material - required material
+
+  //rewrite if (WeaponCalculationParams.nGreen < domainMaterialGreen) {
+  //   WeaponCalculationParams.oGreen =
+  //     WeaponCalculationParams.nGreen - domainMaterialGreen;
+  // }
+
   if (WeaponCalculationParams.nGreen - domainMaterialGreen < 0) {
+    //calculates overflow for green
+    //WeaponCalculationParams.uGreen =0
+    WeaponCalculationParams.uGreen = 0;
     WeaponCalculationParams.oGreen = Math.abs(
       WeaponCalculationParams.nGreen - domainMaterialGreen
     );
   } else {
+    //calculates underflow for green
     WeaponCalculationParams.oGreen = 0;
     WeaponCalculationParams.uGreen =
       WeaponCalculationParams.nGreen - domainMaterialGreen;
   }
   if (WeaponCalculationParams.nBlue - domainMaterialBlue < 0) {
+    //calculates overflow for blue
+    WeaponCalculationParams.uBlue = 0;
     WeaponCalculationParams.oBlue = Math.abs(
       WeaponCalculationParams.nBlue - domainMaterialBlue
     );
   } else {
+    //calculates underflow for blue
     WeaponCalculationParams.oBlue = 0;
     WeaponCalculationParams.uBlue =
       WeaponCalculationParams.nBlue - domainMaterialBlue;
   }
   if (WeaponCalculationParams.nPurple - domainMaterialPurple < 0) {
+    //calculates overflow for purple
+    WeaponCalculationParams.uPurple = 0;
     WeaponCalculationParams.oPurple = Math.abs(
       WeaponCalculationParams.nPurple - domainMaterialPurple
     );
   } else {
+    //calculates underflow for purple
     WeaponCalculationParams.oPurple = 0;
     WeaponCalculationParams.uPurple =
       WeaponCalculationParams.nPurple - domainMaterialPurple;
   }
   if (WeaponCalculationParams.nOrange - domainMaterialOrange < 0) {
+    //calculates overflow for orange
+    WeaponCalculationParams.uOrange = 0;
     WeaponCalculationParams.oOrange = Math.abs(
       WeaponCalculationParams.nOrange - domainMaterialOrange
     );
   } else {
+    //calculates underflow for orange
     WeaponCalculationParams.oOrange = 0;
     WeaponCalculationParams.uOrange =
       WeaponCalculationParams.nOrange - domainMaterialOrange;
   }
-  // calculate what materials can be converted
-  //can't convert green
+  // calculate how many materials can be converted to the next tier
+  //calculates missing (required) materials
+  //can't convert anything to green
   WeaponCalculationParams.mGreen = Math.abs(0 - WeaponCalculationParams.uGreen);
   //top block = underflow greater bottom block = over flow greater
   if (
+    //converted green + overflow blue - underflow blue <=0
     Math.floor(WeaponCalculationParams.oGreen / 3) +
       WeaponCalculationParams.oBlue -
       WeaponCalculationParams.uBlue <=
     0
   ) {
+    //calculates missing (required) blue
     WeaponCalculationParams.pBlue = 0;
     WeaponCalculationParams.mBlue = Math.abs(
       Math.floor(WeaponCalculationParams.oGreen / 3) +
         WeaponCalculationParams.oBlue -
         WeaponCalculationParams.uBlue
     );
+    console.log("mblue" + WeaponCalculationParams.mBlue);
   } else {
+    //calculates potential blue
+    //WeaponCalculationParams.mBlue =0
+    WeaponCalculationParams.mBlue = 0;
+    //number of greens converted to blue - number of missing blue materials
     WeaponCalculationParams.pBlue =
       Math.floor(WeaponCalculationParams.oGreen / 3) +
       WeaponCalculationParams.oBlue -
@@ -183,6 +214,7 @@ export function calculateDomainMaterial(
         WeaponCalculationParams.uPurple
     );
   } else {
+    WeaponCalculationParams.mPurple = 0;
     WeaponCalculationParams.pPurple =
       Math.floor(WeaponCalculationParams.pBlue / 3) +
       WeaponCalculationParams.oPurple -
@@ -201,6 +233,7 @@ export function calculateDomainMaterial(
         WeaponCalculationParams.uOrange
     );
   } else {
+    WeaponCalculationParams.mOrange = 0;
     WeaponCalculationParams.pOrange =
       Math.floor(WeaponCalculationParams.pPurple / 3) +
       WeaponCalculationParams.oOrange -
@@ -239,6 +272,7 @@ export function calculateWeaponMaterial(
   }
   //calculate overflow and underflow
   if (WeaponCalculationParams.nEGreen - eliteMaterialGreen < 0) {
+    WeaponCalculationParams.uEGreen = 0; //
     WeaponCalculationParams.oEGreen = Math.abs(
       WeaponCalculationParams.nEGreen - eliteMaterialGreen
     );
@@ -248,20 +282,22 @@ export function calculateWeaponMaterial(
       WeaponCalculationParams.nEGreen - eliteMaterialGreen;
   }
   if (WeaponCalculationParams.nEBlue - eliteMaterialBlue < 0) {
+    WeaponCalculationParams.uEBlue = 0; //
     WeaponCalculationParams.oEBlue = Math.abs(
       WeaponCalculationParams.nEBlue - eliteMaterialBlue
     );
   } else {
-    WeaponCalculationParams.oEBlue = 0;
+    WeaponCalculationParams.oEBlue = 0; //
     WeaponCalculationParams.uEBlue =
       WeaponCalculationParams.nEBlue - eliteMaterialBlue;
   }
   if (WeaponCalculationParams.nEPurple - eliteMaterialPurple < 0) {
+    WeaponCalculationParams.uEPurple = 0; //
     WeaponCalculationParams.oEPurple = Math.abs(
       WeaponCalculationParams.nEPurple - eliteMaterialPurple
     );
   } else {
-    WeaponCalculationParams.oEPurple = 0;
+    WeaponCalculationParams.oEPurple = 0; //
     WeaponCalculationParams.uEPurple =
       WeaponCalculationParams.nEPurple - eliteMaterialPurple;
   }
@@ -285,6 +321,7 @@ export function calculateWeaponMaterial(
         WeaponCalculationParams.uEBlue
     );
   } else {
+    WeaponCalculationParams.mEBlue = 0;
     WeaponCalculationParams.pEBlue =
       Math.floor(WeaponCalculationParams.oEGreen / 3) +
       WeaponCalculationParams.oEBlue -
@@ -303,6 +340,7 @@ export function calculateWeaponMaterial(
         WeaponCalculationParams.uEPurple
     );
   } else {
+    WeaponCalculationParams.mEPurple = 0;
     WeaponCalculationParams.pEPurple =
       Math.floor(WeaponCalculationParams.pEBlue / 3) +
       WeaponCalculationParams.oEPurple -
@@ -341,6 +379,7 @@ export function calculateCommonMaterial(
   }
   //calculate overflow and underflow
   if (WeaponCalculationParams.nCWhite - commonMaterialWhite < 0) {
+    WeaponCalculationParams.uCWhite = 0; //
     WeaponCalculationParams.oCWhite = Math.abs(
       WeaponCalculationParams.nCWhite - commonMaterialWhite
     );
@@ -350,6 +389,7 @@ export function calculateCommonMaterial(
       WeaponCalculationParams.nCWhite - commonMaterialWhite;
   }
   if (WeaponCalculationParams.nGreen - commonMaterialGreen < 0) {
+    WeaponCalculationParams.uCGreen = 0; //
     WeaponCalculationParams.oCGreen = Math.abs(
       WeaponCalculationParams.nCGreen - commonMaterialGreen
     );
@@ -359,6 +399,7 @@ export function calculateCommonMaterial(
       WeaponCalculationParams.nCGreen - commonMaterialGreen;
   }
   if (WeaponCalculationParams.nCBlue - commonMaterialBlue < 0) {
+    WeaponCalculationParams.uCBlue = 0; //
     WeaponCalculationParams.oCBlue = Math.abs(
       WeaponCalculationParams.nCBlue - commonMaterialBlue
     );
@@ -387,6 +428,7 @@ export function calculateCommonMaterial(
         WeaponCalculationParams.uCGreen
     );
   } else {
+    WeaponCalculationParams.mCGreen = 0; //
     WeaponCalculationParams.pCGreen =
       Math.floor(WeaponCalculationParams.oCWhite / 3) +
       WeaponCalculationParams.oCGreen -
@@ -405,6 +447,7 @@ export function calculateCommonMaterial(
         WeaponCalculationParams.uCBlue
     );
   } else {
+    WeaponCalculationParams.mCBlue = 0; //
     WeaponCalculationParams.pCBlue =
       Math.floor(WeaponCalculationParams.pCGreen / 3) +
       WeaponCalculationParams.oCBlue -
